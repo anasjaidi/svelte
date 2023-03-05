85b11c44674c9e56ecd42972540c10167c910eff
Anas jaidi <anas.jaidi@icloud.com>
2023-03-05 13:24:40 +0100

    listen to input change and activate two value binding

--- firstApp/src/App.svelte
+++ firstApp/src/App.svelte
@@ -1,16 +1,32 @@
 <script>
   let counter = 0;
+  let name = "";
 
   const incButtonClickHandler = e => {
     console.log(e);
     counter++;
   }
 
+  const inputChangeHandler = e => {
+    name = e.target.value
+  }
+
+  const resetButtonHandler = (e) => {
+    e.preventDefault()
+    name = "jaidi"
+  }
 </script>
 
 <main>
   <div>
     <p>counter: {counter}</p>
     <button on:click={incButtonClickHandler}>inc</button>
+  </div>
+  <div>
+    <p>Name: {name}</p>
+    <form>
+      <input type="text" on:input={inputChangeHandler} value={name}/>
+      <button on:click={resetButtonHandler}>reset to jaidi</button>
+    </form>
   </div>
 </main>
