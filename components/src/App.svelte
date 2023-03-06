a644cb2e69af37e7388ac556cca2575ed3556c8f
Anas jaidi <anas.jaidi@icloud.com>
2023-03-06 13:21:15 +0100

    forward events handlers with props

--- components/src/App.svelte
+++ components/src/App.svelte
@@ -2,7 +2,7 @@
     import Product from "./components/Product.svelte";
 </script>
 
-<Product productTitle="product 1" on:click={() => alert("yes")}></Product>
+<Product productTitle="product 1" on:click={() => alert("yes")} deleteHandler="{() => console.log(123)}"></Product>
 
 <style>
 
--- components/src/components/Product.svelte
+++ components/src/components/Product.svelte
@@ -1,11 +1,12 @@
 <script>
     export let productTitle;
+    export let deleteHandler;
 </script>
 
 <article>
     <h1>{productTitle}</h1>
     <button on:click>add on card</button>
-    <button>delete</button>
+    <button on:click={deleteHandler}>delete</button>
 </article>
 
 <style></style>
