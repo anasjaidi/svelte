cb79da48f197df07c33ac3d7fcdcef1d169522a4
Anas jaidi <anas.jaidi@icloud.com>
2023-03-06 09:44:49 +0100

    start on meetupsItem component

--- practice-poroject/src/App.svelte
+++ practice-poroject/src/App.svelte
@@ -1,10 +1,20 @@
 <script>
     import Header from "./UI/Header.svelte";
+
+    let meetups = [
+        {
+            id: "m1",
+            title: "working with svelte",
+            subtitle: "learning svelte in 12 hours",
+            description: "learn how to build spa applications with svelte",
+            address: "27th new york 2347",
+            contactEmail: "anas.jaidi@icloud.com",
+            imageUrl: ""
+        }
+    ]
 </script>
 
 <Header></Header>
-<main>
-</main>
 
 <style>
 
